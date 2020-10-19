a053aa5b7b1e263b0616af5ba536e103f7038a44
김대광 <@DESKTOP-FSRD4MA>
2020-10-19 23:32:13 +0900

    달력 수정...이슈 있음

--- CommonJava8/src/test/java/common/test/TestCreateCalander.java
+++ CommonJava8/src/test/java/common/test/TestCreateCalander.java
@@ -8,25 +8,26 @@ public class TestCreateCalander {
 
 	@Test
 	public void test() {
-		String now_date = "";
-		now_date = Jsr310DateUtil.Today.getTodayString("yyyyMM") + "01";
+		String now_date = "20201101";
+		//now_date = Jsr310DateUtil.Today.getTodayString("yyyyMM") + "01";
 
 		int now_start_day = Jsr310DateUtil.GetDayOfWeek.getFirstDayOfWeek(now_date);
 		int now_last_day = Jsr310DateUtil.GetDayOfMonth.getLastDayOfMonth(now_date);
-
+		
 		StringBuilder sb = new StringBuilder();
 		sb.append(Jsr310DateUtil.StringFormat.getStringDate(now_date, "yyyy.MM"));
 		sb.append("\r\n");
 		sb.append("일").append("\t").append("월").append("\t").append("화").append("\t");
 		sb.append("수").append("\t").append("목").append("\t").append("금").append("\t").append("토");
 
+		// TODO : 1일이 일요일인경우, 이슈...
 		for (int i=1; i < 42; i++) {
 			if (i % 7 == 1) {
 				sb.append("\r\n");
 			}
 
-			int now_day = (i - now_start_day + 1);
-
+			int now_day = (i - now_start_day);
+			
 			if ( (now_day > 0) && (now_day <= now_last_day) ) {
 				if (i % 7 == 0) {
 					sb.append("(").append(now_day).append(")").append("\t");
