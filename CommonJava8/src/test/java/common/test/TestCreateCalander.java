3d2f80073e1cc740e10e59f11e5537448d59167a
김대광 <@DESKTOP-FSRD4MA>
2020-10-20 21:00:21 +0900

    달력 수정

--- CommonJava8/src/test/java/common/test/TestCreateCalander.java
+++ CommonJava8/src/test/java/common/test/TestCreateCalander.java
@@ -8,40 +8,50 @@ public class TestCreateCalander {
 
 	@Test
 	public void test() {
-		String now_date = "20201101";
-		//now_date = Jsr310DateUtil.Today.getTodayString("yyyyMM") + "01";
-
-		int now_start_day = Jsr310DateUtil.GetDayOfWeek.getFirstDayOfWeek(now_date);
-		int now_last_day = Jsr310DateUtil.GetDayOfMonth.getLastDayOfMonth(now_date);
+		String yearMonth = "202011";
+		String yearMonthDay = yearMonth + "01";
+		
+		int dayOfWeek = Jsr310DateUtil.GetDayOfWeek.getFirstDayOfWeek(yearMonthDay);
+		int lastDayOfMonth = Jsr310DateUtil.GetDayOfMonth.getLastDayOfMonth(yearMonthDay);
+		
+		// Joda-Time 과 Java 8의 JSR-310은 Calendar와 다르게 일요일이 7이므로 1로 변경
+		if ( dayOfWeek == 7 ) {
+			dayOfWeek = 1;
+		} else {
+			dayOfWeek = dayOfWeek + 1;
+		}
 		
 		StringBuilder sb = new StringBuilder();
-		sb.append(Jsr310DateUtil.StringFormat.getStringDate(now_date, "yyyy.MM"));
+		sb.append(Jsr310DateUtil.StringFormat.getStringDate(yearMonthDay, "yyyy.MM"));
 		sb.append("\r\n");
 		sb.append("일").append("\t").append("월").append("\t").append("화").append("\t");
 		sb.append("수").append("\t").append("목").append("\t").append("금").append("\t").append("토");
-
-		// TODO : 1일이 일요일인경우, 이슈...
-		for (int i=1; i < 42; i++) {
-			if (i % 7 == 1) {
+		sb.append("\r\n");
+		
+		for (int i=1; i <= lastDayOfMonth; i++) {
+			if (i == 1) {
+				for (int j=1; j < dayOfWeek; j++) {
+					sb.append("\t");
+				}
+			}
+			
+			if ( dayOfWeek % 7 == 0 ) {
+				sb.append("(").append(i).append(")").append("\t");
+			}
+			else if ( dayOfWeek % 7 == 1 ) {
+				sb.append("{").append(i).append("}").append("\t");
+			}
+			else {
+				sb.append(i).append("\t");
+			}
+			
+			if ( dayOfWeek % 7 == 0 ) {
 				sb.append("\r\n");
 			}
-
-			int now_day = (i - now_start_day);
 			
-			if ( (now_day > 0) && (now_day <= now_last_day) ) {
-				if (i % 7 == 0) {
-					sb.append("(").append(now_day).append(")").append("\t");
-				}
-				else if (i % 7 == 1) {
-					sb.append("{").append(now_day).append("}").append("\t");
-				}
-				else {
-					sb.append(now_day).append("\t");
-				}
-			} else {
-				sb.append("\t");
-			}
+			dayOfWeek ++;
 		}
+
 		System.out.println(sb.toString());
 	}
 
