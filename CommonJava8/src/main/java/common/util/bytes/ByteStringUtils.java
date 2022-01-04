522146b4ac04287905f8b5631bfbd5c804edd1ba
KimDaeKwang <kdk1026@theinfoin.com>
2022-01-04 18:18:25 +0900

    전면 수정

--- CommonJava8/src/main/java/common/util/bytes/ByteStringUtils.java
+++ CommonJava8/src/main/java/common/util/bytes/ByteStringUtils.java
@@ -17,18 +17,17 @@ import org.slf4j.LoggerFactory;
  * 개정이력
  * -----------------------------------
  * 2021. 8. 14. 김대광	SonarLint 지시에 따른 수정 및 주저리 (Complexity 어쩔 수 없다)
- * 		훗... 비주얼 베이직 소스 보고 자바로 연구하던 시절이 무릇무릇 떠오르는구만 ㅋㅋㅋ
  * </pre>
  * 
  *
  * @author 김대광
  */
 public class ByteStringUtils {
-	
+
 	private ByteStringUtils() {
 		super();
 	}
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(ByteStringUtils.class);
 
 	/**
@@ -36,7 +35,7 @@ public class ByteStringUtils {
 	 */
 	public static final String UTF_8 = StandardCharsets.UTF_8.toString();
 	public static final String EUC_KR = Charset.forName("EUC-KR").toString();
-	
+
 	/**
 	 * <pre>
 	 * 문자열의 Bytes 길이 구하기
@@ -48,7 +47,7 @@ public class ByteStringUtils {
 	public static int getByteLength(String str) {
 		return str.getBytes().length;
 	}
-	
+
 	/**
 	 * <pre>
 	 * 문자열의 Bytes 길이 구하기
@@ -60,34 +59,34 @@ public class ByteStringUtils {
 	 */
 	public static int getByteLength(String str, String charsetName) {
 		int nLen = 0;
-		
+
 		try {
 			nLen = str.getBytes(charsetName).length;
-			
+
 		} catch (UnsupportedEncodingException e) {
 			logger.error("", e);
 		}
-		
+
 		return nLen;
 	}
-	
+
 	/**
 	 * 문자열 바이트 최대 준수 여부
 	 * @param str
 	 * @param maxByte
 	 * @return
 	 */
-	public static boolean isByteOver(String str, byte maxByte) {
+	public static boolean isByteOver(String str, int maxByte) {
 		boolean resFlag = false;
-		
+
 		try {
 			byte strByte = (byte) str.getBytes(UTF_8).length;
 			resFlag = strByte > maxByte;
-			
+
 		} catch (Exception e) {
 			logger.error("", e);
 		}
-		
+
 		return resFlag;
 	}
 
@@ -98,327 +97,129 @@ public class ByteStringUtils {
 	 * @param charsetName
 	 * @return
 	 */
-	public static boolean isByteOver(String str, byte maxByte, String charsetName) {
+	public static boolean isByteOver(String str, int maxByte, String charsetName) {
 		boolean resFlag = false;
-		
+
 		try {
 			byte strByte = (byte) str.getBytes(charsetName).length;
 			resFlag = strByte > maxByte;
-			
+
 		} catch (Exception e) {
 			logger.error("", e);
 		}
-		
+
 		return resFlag;
 	}
-	
+
 	/**
-	 * 해당 케릭터셋으로 변환
-	 * @param bData
+	 * euc-kr 바이트 어레이를 utf-8 문자열로 변환
+	 * @param bOrgData
+	 * @return
+	 */
+	public static String eucKrToUtf8String(byte[] bOrgData) {
+		String sRes = "";
+
+		try {
+			String str = new String(bOrgData, EUC_KR);
+			byte[] bData = str.getBytes(UTF_8);
+			sRes = new String(bData, UTF_8);
+
+		} catch (UnsupportedEncodingException e) {
+			logger.error("", e);
+		}
+
+		return sRes;
+	}
+
+	/**
+	 * utf-8 바이트 어레이를 euc-kr 문자열로 변환
+	 * @param bOrgData
+	 * @return
+	 */
+	public static String utf8ToEucKrString(byte[] bOrgData) {
+		String sRes = "";
+
+		try {
+			String str = new String(bOrgData, UTF_8);
+			byte[] bData = str.getBytes(EUC_KR);
+			sRes = new String(bData, EUC_KR);
+
+		} catch (UnsupportedEncodingException e) {
+			logger.error("", e);
+		}
+
+		return sRes;
+	}
+
+	/**
+	 * euc-kr 바이트 어레이를 utf-8 바이트 어레이로 변환
+	 * @param bOrgData
+	 * @return
+	 */
+	public static byte[] eucKrToUtf8(byte[] bOrgData) {
+		byte[] bData = null;
+
+		try {
+			String str = new String(bOrgData, EUC_KR);
+			bData = str.getBytes(UTF_8);
+
+		} catch (UnsupportedEncodingException e) {
+			logger.error("", e);
+		}
+
+		return bData;
+	}
+
+	/**
+	 * utf-8 바이트 어레이를 euc-kr 바이트 어에이로 변환
+	 * @param bOrgData
+	 * @return
+	 */
+	public static byte[] utf8ToEucKr(byte[] bOrgData) {
+		byte[] bData = null;
+
+		try {
+			String str = new String(bOrgData, UTF_8);
+			bData = str.getBytes(EUC_KR);
+
+		} catch (UnsupportedEncodingException e) {
+			logger.error("", e);
+		}
+
+		return bData;
+	}
+
+	/**
+	 * byte 단위로 문자열 자르기
+	 * @param str
+	 * @param offset
+	 * @param length
 	 * @param charsetName
 	 * @return
 	 */
-	public static byte[] toByteEncoding(byte[] bData, String charsetName) {
-		String sData = "";
-		byte[] bResData = null;
-		
-		try {
-			sData = new String(bData, charsetName);
-			bResData = sData.getBytes(charsetName); 
-			
-		} catch (UnsupportedEncodingException e) {
-			logger.error("", e);
-		}
-		
-		return bResData;
-	}
-
-	/**
-	 * byte 단위로 문자열 자르기
-	 * @param sData
-	 * @param nStartByte
-	 * @param nEndByte
-	 * @param isUtf8
-	 * @return
-	 */
-	public static String getByteStrSubstring(String sData, int nStartByte, int nCutByte, boolean isUtf8) {
-		int nBeginIndex = -1;
-		int nEndIndex = -1;
-		int nChkByte = 0;
-		int nEndByte = nStartByte + nCutByte;
-		
-		byte[] bByte = null;
-		int nByteLen = 0;
-		
-		try {
-			bByte = sData.getBytes(EUC_KR);
-			nByteLen = bByte.length;
-
-			// XXX : 확실치 않음...
-			if (nEndByte > nByteLen) {
-				nEndByte = (isUtf8) ? nByteLen - 1 : nByteLen - 2;
-			}
-			
-		} catch (UnsupportedEncodingException e) {
-			logger.error("", e);
-		}
-		
-		for (int i=0; i < sData.length(); i++) {
-			String ch = Character.toString(sData.charAt(i));
-			
-			if ( isUtf8 ) {
-				nChkByte += ch.getBytes().length >= 2 ? 3 : 1;
-			} else {
-				nChkByte += ch.getBytes().length >= 2 ? 2 : 1;
-			}
-			
-			if ( nBeginIndex == -1 && nChkByte >= nStartByte ) {
-				nBeginIndex = i;
-			}
-			
-			if ( nEndIndex == -1 && nChkByte >= nEndByte ) {
-				nEndIndex = (nChkByte > nEndByte) ? i : i+2;
-				break;
-			}
-		}
-		
-		return sData.substring(nBeginIndex, nEndIndex);
-	}
-	
-	/**
-	 * <pre>
-	 * 문자열의 해당 위치까지 문자의 영/한 여부 체크
-	 *   - 0:영문, 1:한글 첫번째 Byte, 2:한글 두번째 Byte
-	 * </pre>
-	 * @param sData
-	 * @param nChkPos
-	 * @return
-	 */
-	public static int checkByteEucKr(String sData, int nChkPos) {
-		int nChkByte = 0;
-		byte[] bByte = null;
-		int nByteLen = 0;
-		
-		try {
-			bByte = sData.getBytes(EUC_KR);
-			nByteLen = bByte.length;
-			
-			int i;
-			for (i=0; i < nChkPos; i++) {
-				if (nChkPos > nByteLen) {
-					break;
-				}
-				
-				char c = (char) bByte[i];
-				if ( c > 127 ) {
-					nChkByte = 1;
-					i++;
-				} else {
-					nChkByte = 0;
-				}
-			}
-			
-			
-			if ( nChkByte == 1 && (i-1) == (nChkPos-1) ) {
-				nChkByte = 2;
-			}
-			
-		} catch (Exception e) {
-			logger.error("", e);
-		}
-		
-		return nChkByte;
-	}
-	
-	/**
-	 * byte 단위로 문자열 자르기
-	 *   - subStrByteBuffer 보다 다소 성능 ↓ 
-	 * </pre>
-	 * @param sData
-	 * @param nStart
-	 * @param nLen
-	 * @return
-	 */
-	public static String subStrByteEucKr(String sData, int nStart, int nLen) {
-		byte[] bByte = null;
-		int nByteLen = 0;
-		int nChkByte = 0;
-		int nStartIdx = 0;
-		int nEndIdx = 0;
+	public static String substrString(String str, int offset, int length, String charsetName) {
 		String sRes = "";
-		
-		try {
-			bByte = sData.getBytes(EUC_KR);
-			nByteLen = bByte.length;
 
-			nStartIdx = nStart;
-			nEndIdx = nStart + nLen;
-			
-			if (nEndIdx > nByteLen) {
-				nEndIdx = nByteLen - nLen;
+		try {
+			byte[] bytes = str.getBytes(charsetName);
+
+			byte[] value = new byte[length];
+
+			if (bytes.length < offset + length) {
+				return "";
 			}
-			
-			if (nEndIdx >= nByteLen || nLen == 0) {
-				nEndIdx = nByteLen;
+
+			for(int i = 0; i < length; i++){
+				value[i] = bytes[offset + i];
 			}
-			
-			nChkByte = checkByteEucKr(sData, nStart);
-			switch (nChkByte) {
-			case 1:
-				nStartIdx = nStartIdx - 1;
-				break;
-			case 2:
-				nStartIdx = nStartIdx - 2;
-				break;
-			default:
-				break;
-			}
-			
-			nChkByte = checkByteEucKr(sData, nEndIdx);
-			
-			// XXX : 확실치 않음...
-			switch (nChkByte) {
-			case 1:
-				nEndIdx = (nStart == 0) ? nEndIdx - 1 : nEndIdx + 1;
-				break;
-			case 2:
-				nEndIdx = (nStart == 0) ? nEndIdx - 2 : nEndIdx + 2;
-				break;
-			default:
-				break;
-			}
-			
-			sRes = new String(bByte, nStartIdx, nEndIdx, EUC_KR);
-			
+
+			sRes = new String(value, charsetName).trim();
+
 		} catch (UnsupportedEncodingException e) {
 			logger.error("", e);
 		}
-		
+
 		return sRes;
 	}
-	
-	/**
-	 * <pre>
-	 * byte 단위로 문자열 자르기
-	 *   - subStrByte 보다 다소 성능 ↑
-	 * </pre>
-	 * @param sData
-	 * @param nStart
-	 * @param nLen
-	 * @return
-	 */
-	public static String subStrByteBufferEucKr(String sData, int nStart, int nLen) {
-		final String sEncoding = EUC_KR;
-		
-		int nByteLen = 0;
-		int nChkByte = 0;
-		int nStartIdx = 0;
-		int nEndIdx = 0;
-		String sRes = "";
-		
-		try {
-			Charset charset =  Charset.forName(sEncoding);
-			CharsetEncoder encoder = charset.newEncoder();
-			ByteBuffer bBuf = null;
-			CharBuffer cBuf = null;
-			
-			bBuf = encoder.encode(CharBuffer.wrap(sData));
-			
-			nByteLen = bBuf.limit();
-			
-			nStartIdx = nStart;
-			nEndIdx = nStart + nLen;
-			
-			if (nEndIdx > nByteLen) {
-				nEndIdx = nByteLen - nLen;
-			}
-			
-			if (nEndIdx >= nByteLen || nLen == 0) {
-				nEndIdx = nByteLen;
-			}
-			
-			nChkByte = checkByteEucKr(sData, nStart);
-			switch (nChkByte) {
-			case 1:
-				nStartIdx = nStartIdx - 1;
-				break;
-			case 2:
-				nStartIdx = nStartIdx - 2;
-				break;
-			default:
-				break;
-			}
-			
-			nChkByte = checkByteEucKr(sData, nEndIdx);
-			// XXX : 확실치 않음...
-			switch (nChkByte) {
-			case 1:
-				nEndIdx = (nStart == 0) ? nEndIdx - 1 : nEndIdx + 1;
-				break;
-			case 2:
-				nEndIdx = (nStart == 0) ? nEndIdx - 2 : nEndIdx + 2;
-				break;
-			default:
-				break;
-			}
-			
-			bBuf.position(nStartIdx);
-			bBuf.limit(bBuf.position() + nEndIdx);
-			
-			cBuf = charset.decode(bBuf);
-			
-			sRes = cBuf.toString();
-			
-		} catch (Exception e) {
-			logger.error("", e);
-		}
-		
-		return sRes;
-	}
-	
-	/**
-	 * byte 단위로 문자열 잘라서 List로 반환
-	 * @param sData
-	 * @param nLimit
-	 * @param charsetName
-	 * @since 1.7
-	 * @return
-	 */
-	// TODO : 확인 > 수정 필요
-	public static List<String> subStrByteBuffer(String sData, int nLimit, String charsetName) {
-		List<String> resList = new ArrayList<>();
-		String str = "";
-		ByteBuffer buf = null;
-		
-		try {
-			buf = ByteBuffer.wrap(sData.getBytes(charsetName));
-		} catch (UnsupportedEncodingException e) {
-			logger.error("", e);
-		}
-		
-		if (buf != null) {
-			int nLen = buf.limit();
-			int nPos = 0;
-			byte[] bResData = null;
-			
-			while (nPos < nLen) {
-				buf.position(nPos);
-				buf.limit(buf.position() + nLimit);
-				bResData = new byte[buf.remaining()];
-				buf.get(bResData);
-				
-				try {
-					str = new String(bResData, charsetName).trim();
-				} catch (UnsupportedEncodingException e) {
-					logger.error("", e);
-				}
-				resList.add(str);
-				
-				nPos = nPos + nLimit;
-			}
-		}
-		
-		return resList;
-	}	
-	
+
 }
