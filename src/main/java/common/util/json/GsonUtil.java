60ba654a0672a77450f66172b11ec0a7dc76bc6f
kdk1026 <kdk1026@gmail.com>
2024-06-11 19:54:52 +0900

    JSON 유틸 수정

--- src/main/java/common/util/json/GsonUtil.java
+++ src/main/java/common/util/json/GsonUtil.java
@@ -21,25 +21,25 @@ import com.google.gson.stream.JsonReader;
  * 작은 파일에 유리
  */
 public class GsonUtil {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(GsonUtil.class);
-	
+
 	private GsonUtil() {
 		super();
 	}
-	
+
 	private static Gson getInstance() {
 		// new Gson();	- 유니코드 지원안함
 		return new GsonBuilder().disableHtmlEscaping().create();
 	}
-	
+
 	public static String prettyPrintString(String jsonStr) {
 		Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
 		return gson.toJson(jsonStr);
 	}
-	
+
 	public static class ToJson {
-		
+
 		protected ToJson() {
 			super();
 		}
@@ -48,27 +48,27 @@ public class GsonUtil {
 			String sJson = "";
 
 			Gson gson = getInstance();
-			
+
 			try {
 				sJson = gson.toJson(obj);
 			} catch (Exception e) {
 				logger.error("", e);
 			}
-			
+
 			return sJson;
 		}
-		
+
 		public static String converterMapToJsonStr(Map<String, Object> map) {
 			return converterObjToJsonStr(map);
 		}
 
-		public static String converterListToJsonStr(List<Map<String, Object>> list) {
+		public static String converterListToJsonStr(List<?> list) {
 			return converterObjToJsonStr(list);
-		}		
+		}
 	}
-	
+
 	public static class FromJson {
-		
+
 		protected FromJson() {
 			super();
 		}
@@ -78,57 +78,68 @@ public class GsonUtil {
 			Map<String, Object> map = new HashMap<>();
 
 			Gson gson = getInstance();
-			
+
 			try {
 				map = gson.fromJson(sJson, Map.class);
 			} catch (Exception e) {
 				logger.error("", e);
 			}
-			
+
 			return map;
 		}
-		
+
 		public static JsonObject converterJsonStrToJsonObj(String sJson) {
 			JsonObject jsonObj = null;
-			
+
 			Gson gson = getInstance();
-			
+
 			try {
 				jsonObj = gson.fromJson(sJson, JsonObject.class);
 			} catch (Exception e) {
 				logger.error("", e);
 			}
-			
+
 			return jsonObj;
 		}
-		
-		@SuppressWarnings("unchecked")
-		public static List<Map<String, Object>> converterJsonStrToList(String sJsonArr) {
-			List<Map<String, Object>> list = new ArrayList<>();
+
+		public static List<?> converterJsonStrToList(String sJsonArr) {
+			List<?> list = new ArrayList<>();
 
 			Gson gson = getInstance();
-			
+
 			try {
 				list = gson.fromJson(sJsonArr, List.class);
 			} catch (Exception e) {
 				logger.error("", e);
 			}
-			
+
 			return list;
 		}
-		
+
 		public static JsonArray converterJsonStrToJsonArray(String sJsonArr) {
 			JsonArray jsonArray = null;
-			
+
 			Gson gson = getInstance();
-			
+
 			try {
 				jsonArray = gson.fromJson(sJsonArr, JsonArray.class);
 			} catch (Exception e) {
 				logger.error("", e);
 			}
-			
+
 			return jsonArray;
+		}
+
+		public static <T> T converterJsonStrToClass(String jsonStr, Class<T> clazz) {
+			Gson gson = getInstance();
+
+			try {
+				Object result = gson.fromJson(jsonStr, clazz);
+				return clazz.cast(result);
+			} catch (Exception e) {
+				logger.error("", e);
+			}
+			return null;
 		}
 	}
 
@@ -137,35 +148,35 @@ public class GsonUtil {
 		protected ReadJsonFile() {
 			super();
 		}
-		
+
 		public static Object readJsonFileObject(String sfileName, Type type) {
 			Object obj = null;
 			Gson gson = getInstance();
-			
+
 			try {
 				JsonReader reader = new JsonReader(new FileReader(sfileName));
 				obj = gson.fromJson(reader, type);
 			} catch (FileNotFoundException e) {
 				logger.error("", e);
 			}
-			
+
 			return obj;
 		}
-		
+
 		@SuppressWarnings("unchecked")
 		public static <T> List<T> readJsonFileArray(String sfileName, Type type) {
 			Object obj = null;
 			Gson gson = getInstance();
-			
+
 			try {
 				JsonReader reader = new JsonReader(new FileReader(sfileName));
 				obj = gson.fromJson(reader, type);
 			} catch (FileNotFoundException e) {
 				logger.error("", e);
 			}
-			
+
 			return (List<T>) obj;
-		}		
+		}
 	}
-	
+
 }
--- src/main/java/common/util/json/JacksonUtil.java
+++ src/main/java/common/util/json/JacksonUtil.java
@@ -65,7 +65,7 @@ public class JacksonUtil {
 			return jsonStr;
 		}
 
-		public static String converterListToJsonStr(List<Map<String, Object>> list) {
+		public static String converterListToJsonStr(List<?> list) {
 			String jsonStr = "";
 
 			ObjectMapper mapper = new ObjectMapper();
@@ -82,7 +82,7 @@ public class JacksonUtil {
 			return mapper.valueToTree(map);
 		}
 
-		public static JsonNode converterListToJsonNode(List<Map<String, Object>> list) {
+		public static JsonNode converterListToJsonNode(List<?> list) {
 			ObjectMapper mapper = new ObjectMapper();
 			return mapper.valueToTree(list);
 		}
@@ -119,9 +119,8 @@ public class JacksonUtil {
 			return jsonNode;
 		}
 
-		@SuppressWarnings("unchecked")
-		public static List<Map<String, Object>> converterJsonStrToList(String jsonArrStr) {
-			List<Map<String, Object>> list = new ArrayList<>();
+		public static List<?> converterJsonStrToList(String jsonArrStr) {
+			List<?> list = new ArrayList<>();
 
 			ObjectMapper mapper = new ObjectMapper();
 			try {
@@ -142,6 +141,18 @@ public class JacksonUtil {
 				logger.error("", e);
 			}
 			return arrayNode;
+		}
+
+		public static <T> T converterJsonStrToClass(String jsonStr, Class<T> clazz) {
+			ObjectMapper mapper = new ObjectMapper();
+
+			try {
+				Object result = mapper.readValue(jsonStr, clazz);
+				return clazz.cast(result);
+			} catch (Exception e) {
+				logger.error("", e);
+			}
+			return null;
 		}
 	}
 
