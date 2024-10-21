b7d406fee895bea217ad953a54875dd080f10226
kdk1026 <kdk1026@gmail.com>
2024-10-21 19:53:54 +0900

    간략하게 수정

--- src/main/java/common/util/RequestUtil.java
+++ src/main/java/common/util/RequestUtil.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
  * 2020. 9. 18. 김대광	최초작성
  * 2021. 8. 13. 김대광	SonarLint 지시에 따른 수정 (Complexity는 어쩔 수 없단다)
  * </pre>
- * 
+ *
  *
  * @author 김대광
  */
@@ -18,52 +18,35 @@ public class RequestUtil {
 	private RequestUtil() {
 		super();
 	}
-	
-	private static final String UNKNOW = "unknown";
-	
+
 	/**
 	 * IP 주소 가져오기
 	 * @param request
 	 * @return
 	 */
 	public static String getRequestIpAddress(HttpServletRequest request) {
-		String sIpAddr = request.getHeader("X-Forwarded-For");
-		
-		if (sIpAddr == null || sIpAddr.length() == 0 || UNKNOW.equalsIgnoreCase(sIpAddr)) {
-			sIpAddr = request.getHeader("Proxy-Client-IP");
-		}
+	    String[] sHeaders = {
+	    		"X-Forwarded-For",
+	    		"Proxy-Client-IP",
+	    		"WL-Proxy-Client-IP",
+	    		"HTTP_CLIENT_IP",
+	    		"HTTP_X_FORWARDED_FOR",
+	    		"X-Real-IP",
+	    		"X-RealIP",
+	    		"REMOTE_ADDR"
+	    };
 
-		if (sIpAddr == null || sIpAddr.length() == 0 || UNKNOW.equalsIgnoreCase(sIpAddr)) {
-			sIpAddr = request.getHeader("WL-Proxy-Client-IP");
-		}
+	    for ( String header : sHeaders ) {
+	    	String sIp = request.getHeader(header);
 
-		if (sIpAddr == null || sIpAddr.length() == 0 || UNKNOW.equalsIgnoreCase(sIpAddr)) {
-			sIpAddr = request.getHeader("HTTP_CLIENT_IP");
-		}
+	    	if ( sIp != null && sIp.length() != 0 && !"unknown".equalsIgnoreCase(sIp) ) {
+	    		return sIp;
+	    	}
+	    }
 
-		if (sIpAddr == null || sIpAddr.length() == 0 || UNKNOW.equalsIgnoreCase(sIpAddr)) {
-			sIpAddr = request.getHeader("HTTP_X_FORWARDED_FOR");
-		}
-		
-		if (sIpAddr == null || sIpAddr.length() == 0 || UNKNOW.equalsIgnoreCase(sIpAddr)) { 
-			sIpAddr = request.getHeader("X-Real-IP"); 
-        }
-		
-		if (sIpAddr == null || sIpAddr.length() == 0 || UNKNOW.equalsIgnoreCase(sIpAddr)) { 
-			sIpAddr = request.getHeader("X-RealIP"); 
-		}
-		
-		if (sIpAddr == null || sIpAddr.length() == 0 || UNKNOW.equalsIgnoreCase(sIpAddr)) { 
-			sIpAddr = request.getHeader("REMOTE_ADDR"); 
-		}
-
-		if (sIpAddr == null || sIpAddr.length() == 0 || UNKNOW.equalsIgnoreCase(sIpAddr)) {
-			sIpAddr = request.getRemoteAddr();
-		}
-		
-		return sIpAddr;
+	    return request.getRemoteAddr();
 	}
-	
+
 	/**
 	 * 도메인 가져오기 (포트 있는 경우, 포트 포함)
 	 * @param request
@@ -74,5 +57,5 @@ public class RequestUtil {
 		String sServletPath = request.getServletPath();
 		return sReqUrl.replace(sServletPath, "");
 	}
-	
+
 }
