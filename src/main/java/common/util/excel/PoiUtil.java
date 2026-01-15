171d8c87d366f9aef49394e5a6531fd94737ff96
kdk1026 <daekwang1026@gmail.com>
2026-01-15 20:03:26 +0900

    업데이트

--- src/main/java/common/util/excel/PoiUtil.java
+++ src/main/java/common/util/excel/PoiUtil.java
@@ -20,6 +20,7 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.hssf.util.HSSFColor.HSSFColorPredefined;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.FillPatternType;
 import org.apache.poi.ss.usermodel.Font;
 import org.apache.poi.ss.usermodel.Row;
@@ -37,6 +38,7 @@ import org.slf4j.LoggerFactory;
  * 2021. 8. 13. 김대광	SonarLint 지시에 따른 주저리 주저리
  * 2022. 5. 25. 김대광	deprecated 수정
  * 2026. 1. 12. 김대광	ServletPoiUtil 으로 일부 분리
+ * 2026. 1. 15. 김대광	AI 도움으로 메소드 분리
  * </pre>
  *
  * @author 김대광
@@ -74,206 +76,79 @@ public class PoiUtil {
 			throw new IllegalArgumentException(ExceptionMessage.isNullOrEmpty("cellNames"));
 		}
 
-		Objects.requireNonNull(isDecimal, ExceptionMessage.isNull("isDecimal"));
-
-		List<Map<String, Object>> resList = new ArrayList<>();
-
-		String sFileName = file.getName();
-		String sFileExt = sFileName.substring(sFileName.lastIndexOf('.') + 1);
-
 		try ( InputStream is = new BufferedInputStream(new FileInputStream(file)) ) {
-			Workbook workbook = null;
+			Workbook workbook = createWorkbook(is, file.getName());
 
-			switch (sFileExt) {
-			case "xls":
-				workbook = new HSSFWorkbook(is);
-				break;
-
-			case "xlsx":
-				workbook = new XSSFWorkbook(is);
-				break;
-
-			default:
-				break;
-			}
-
-			if ( workbook == null ) {
+			if ( workbook == null || workbook.getNumberOfSheets() == 0 ) {
 				return Collections.emptyList();
 			}
 
-			if ( workbook.getNumberOfSheets() == 0 ) {
-				return Collections.emptyList();
-			}
+			return parseSheet(workbook.getSheetAt(0), cellNames, isDecimal);
 
-			Sheet sheet = workbook.getSheetAt(0);
-			int nRowCnt = sheet.getPhysicalNumberOfRows();
-
-			for (int rowIdx=0; rowIdx < nRowCnt; rowIdx++) {
-				Row row = sheet.getRow(rowIdx+1);
-
-				if (row == null) {
-					continue;
-				}
-
-				int nCellCnt = row.getPhysicalNumberOfCells();
-				Map<String, Object> map = new HashMap<>();
-
-				for (int cellIdx=0; cellIdx < nCellCnt; cellIdx++) {
-					Cell cell = row.getCell(cellIdx);
-
-					if (cell == null) {
-						continue;
-					}
-
-					Object obj = null;
-
-					switch (cell.getCellTypeEnum()) {
-					case BLANK:
-						obj = "";
-						break;
-
-					case NUMERIC:
-						double numericValue = cell.getNumericCellValue();
-						obj = isDecimal ? numericValue : (int) numericValue;
-						break;
-
-					case STRING:
-						obj = cell.getStringCellValue();
-						break;
-
-					case FORMULA:
-						obj = cell.getCellFormula();
-						break;
-
-					case BOOLEAN:
-						obj = cell.getBooleanCellValue();
-						break;
-
-					case ERROR:
-						obj = cell.getErrorCellValue();
-						break;
-
-					default:
-						break;
-					}
-
-					String sCellNm = "";
-
-					if ( cellNames != null ) {
-						sCellNm = cellNames[cellIdx];
-					} else {
-						sCellNm = cell.getSheet().getRow(0).getCell(cellIdx).getRichStringCellValue().toString();
-					}
-
-					map.put(sCellNm, obj);
-				}
-
-				resList.add(map);
-			}
 
 		} catch (IOException e) {
 			logger.error("", e);
+			return Collections.emptyList();
 		}
+	}
+
+	private static Workbook createWorkbook(InputStream is, String fileName) throws IOException {
+		if (fileName.endsWith(".xls")) {
+	        return new HSSFWorkbook(is);
+	    } else if (fileName.endsWith(".xlsx")) {
+	        return new XSSFWorkbook(is);
+	    }
+
+		return null;
+	}
+
+	private static List<Map<String, Object>> parseSheet(Sheet sheet, String[] cellNames, boolean isDecimal) {
+		List<Map<String, Object>> resList = new ArrayList<>();
+		int nRowCnt = sheet.getPhysicalNumberOfRows();
+
+		for (int rowIdx = 1; rowIdx < nRowCnt; rowIdx++) {
+			Row row = sheet.getRow(rowIdx);
+			if (row == null) continue;
+
+			// 행 단위 처리
+			Map<String, Object> map = new HashMap<>();
+			int nCellCnt = row.getPhysicalNumberOfCells();
+
+			for (int cellIdx = 0; cellIdx < nCellCnt; cellIdx++) {
+				if (cellIdx >= cellNames.length) break;
+
+				Cell cell = row.getCell(cellIdx);
+				String columnName = cellNames[cellIdx];
+				map.put(columnName, getCellValue(cell, isDecimal));
+			}
+
+			resList.add(map);
+		}
+
 		return resList;
 	}
 
-	/**
-	 * 컨텐츠로부터 Workbook 생성
-	 * @param fileName
-	 * @param contentsList
-	 * @param cellTitles
-	 * @return
-	 */
-	private static Workbook createWorkbookFromContents(String fileName, List<Map<String, Object>> contentsList, String[] cellTitles) {
-		if ( StringUtils.isBlank(fileName) ) {
-			throw new IllegalArgumentException(ExceptionMessage.isNullOrEmpty("fileName"));
-		}
+	private static Object getCellValue(Cell cell, boolean isDecimal) {
+		if (cell == null || cell.getCellTypeEnum() == CellType.BLANK) {
+	        return "";
+	    }
 
-		if ( contentsList == null || contentsList.isEmpty() ) {
-			throw new IllegalArgumentException(ExceptionMessage.isNullOrEmpty("contentsList"));
-		}
-
-		if ( cellTitles == null || cellTitles.length == 0 ) {
-			throw new IllegalArgumentException(ExceptionMessage.isNullOrEmpty("cellTitles"));
-		}
-
-		String sFileExt = fileName.substring(fileName.lastIndexOf('.') + 1);
-
-		Workbook workbook = null;
-
-		switch (sFileExt) {
-		case "xls":
-			workbook = new HSSFWorkbook();
-			break;
-
-		case "xlsx":
-			workbook = new XSSFWorkbook();
-			break;
+		switch (cell.getCellTypeEnum()) {
+		case NUMERIC:
+			double val = cell.getNumericCellValue();
+            return isDecimal ? val : (int) val;
+		case STRING:
+			return cell.getStringCellValue();
+		case FORMULA:
+			return cell.getCellFormula();
+		case BOOLEAN:
+			return cell.getBooleanCellValue();
+		case ERROR:
+			return cell.getErrorCellValue();
 
 		default:
-			break;
+			return "";
 		}
-
-		if ( workbook == null ) {
-			return null;
-		}
-
-		Sheet sheet = workbook.createSheet();
-		Row row = null;
-
-		Font font = workbook.createFont();
-		font.setBold(true);
-
-		CellStyle cellStyle = workbook.createCellStyle();
-
-		cellStyle.setFillForegroundColor(HSSFColorPredefined.GREY_25_PERCENT.getIndex());
-		cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-		cellStyle.setFont(font);
-
-		int nCellCnt = 0;
-
-		// 타이틀
-		row = sheet.createRow(0);
-
-		for ( String sCellTitle : cellTitles ) {
-			Cell rowCell = row.createCell(nCellCnt);
-
-			rowCell.setCellStyle(cellStyle);
-			rowCell.setCellValue(sCellTitle);
-			nCellCnt = nCellCnt + 1;
-		}
-
-		// 내용
-		for (int i=0; i < contentsList.size(); i++) {
-			nCellCnt = 0;
-
-			row = sheet.createRow(i+1);
-			Map<String, Object> dataMap = contentsList.get(i);
-
-			for (Map.Entry<String, Object> entry : dataMap.entrySet()) {
-				Object value = entry.getValue();
-
-				if ( value == null ) {
-					row.createCell(nCellCnt).setCellValue("");
-				}
-				else if ( value instanceof String ) {
-					row.createCell(nCellCnt).setCellValue((String) value);
-				}
-				else if ( value instanceof Integer ) {
-					row.createCell(nCellCnt).setCellValue((Integer) value);
-				}
-				else if ( value instanceof Boolean ) {
-					row.createCell(nCellCnt).setCellValue((Boolean) value);
-				}
-				else {
-					row.createCell(nCellCnt).setCellValue(String.valueOf(value));
-				}
-
-				nCellCnt = nCellCnt + 1;
-			}
-		}
-
-		return workbook;
 	}
 
 	/**
@@ -301,23 +176,106 @@ public class PoiUtil {
 			throw new IllegalArgumentException(ExceptionMessage.isNullOrEmpty("cellTitles"));
 		}
 
-		boolean isSuccess = false;
-
 		Workbook workbook = createWorkbookFromContents(fileName, contentsList, cellTitles);
 
-		try {
-			File outFile = new File(destFilePath + File.separator + fileName);
-			OutputStream os = new BufferedOutputStream(new FileOutputStream(outFile));
-			workbook.write(os);
-
-			os.close();
-			isSuccess = true;
-
+		try (
+				FileOutputStream fis = new FileOutputStream(new File(destFilePath + File.separator + fileName));
+				OutputStream os = new BufferedOutputStream(fis)
+		) {
+		    workbook.write(os);
+		    return true;
 		} catch (IOException e) {
-			logger.error("", e);
+		    logger.error("", e);
+		    return false;
+		}
+	}
+
+	/**
+	 * 컨텐츠로부터 Workbook 생성
+	 * @param fileName
+	 * @param contentsList
+	 * @param cellTitles
+	 * @return
+	 */
+	private static Workbook createWorkbookFromContents(String fileName, List<Map<String, Object>> contentsList, String[] cellTitles) {
+		if ( StringUtils.isBlank(fileName) ) {
+			throw new IllegalArgumentException(ExceptionMessage.isNullOrEmpty("fileName"));
 		}
 
-		return isSuccess;
+		if ( contentsList == null || contentsList.isEmpty() ) {
+			throw new IllegalArgumentException(ExceptionMessage.isNullOrEmpty("contentsList"));
+		}
+
+		if ( cellTitles == null || cellTitles.length == 0 ) {
+			throw new IllegalArgumentException(ExceptionMessage.isNullOrEmpty("cellTitles"));
+		}
+
+		Workbook workbook = createEmptyWorkbook(fileName);
+		if ( workbook == null ) {
+			return null;
+		}
+
+		Sheet sheet = workbook.createSheet();
+
+		createHeaderRow(sheet, cellTitles, createHeaderStyle(workbook));
+
+		fillSheetWithData(sheet, contentsList, cellTitles);
+
+		return workbook;
+	}
+
+	private static Workbook createEmptyWorkbook(String fileName) {
+		if (fileName.endsWith(".xls")) {
+			return new HSSFWorkbook();
+	    } else if (fileName.endsWith(".xlsx")) {
+	    	return new XSSFWorkbook();
+	    }
+
+	    return null;
+	}
+
+	private static CellStyle createHeaderStyle(Workbook workbook) {
+		Font font = workbook.createFont();
+	    font.setBold(true);
+
+	    CellStyle style = workbook.createCellStyle();
+	    style.setFillForegroundColor(HSSFColorPredefined.GREY_25_PERCENT.getIndex());
+	    style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+	    style.setFont(font);
+	    return style;
+	}
+
+	private static void createHeaderRow(Sheet sheet, String[] titles, CellStyle style) {
+	    Row headerRow = sheet.createRow(0);
+	    for (int i = 0; i < titles.length; i++) {
+	        Cell cell = headerRow.createCell(i);
+	        cell.setCellStyle(style);
+	        cell.setCellValue(titles[i]);
+	    }
+	}
+
+	private static void fillSheetWithData(Sheet sheet, List<Map<String, Object>> contentsList, String[] cellTitles) {
+	    for (int i = 0; i < contentsList.size(); i++) {
+	        Row row = sheet.createRow(i + 1);
+	        Map<String, Object> dataMap = contentsList.get(i);
+
+	        for (int j = 0; j < cellTitles.length; j++) {
+	            Object value = dataMap.get(cellTitles[j]);
+	            Cell cell = row.createCell(j);
+
+	            if (value == null) {
+	                cell.setCellValue("");
+	            } else if (value instanceof Integer) {
+	                cell.setCellValue((Integer) value);
+	            } else if (value instanceof Boolean) {
+	                cell.setCellValue((Boolean) value);
+	            } else if (value instanceof Double) {
+	                cell.setCellValue((Double) value);
+	            } else {
+	                cell.setCellValue(String.valueOf(value));
+	            }
+	        }
+	    }
 	}
 
 }
